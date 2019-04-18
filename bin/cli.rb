fa35477c1c70fb714585f0df6b1927326fe0680c
github username <github email address>
2019-04-18 10:00:48 +0100

    initial code

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,48 +1,69 @@
 class CLI
 
   def initialize
-    @prompt = TTy::Prompt.new
+    @prompt = TTY::Prompt.new
+    @user = nil
   end
 
-def find_or_create_user
-  name =@prompt.ask("What's your name?: ")
-  @user = User.find_or_create_by(name: name)
-end
+  def find_or_create_user
+    name = @prompt.ask("What's your name?: ")
+    @user = User.find_or_create_by(name: name)
+  end
 
   def welcome
-    puts "Welcome, #{@user}!"
+    puts "Welcome, #{@user.name}!"
   end
 
   def movieselection
-    puts 'Please pick a movie'
-  input =  prompt.select("Choose your destiny?", %w(v for vendetta, atonement, road to pedition, wild target, happy gilmore, madagascar))
-
-    if input == 'titanic'
-     elsif input == 'v for vendetta'
-     elsif input == 'atonement'
-     elsif input == 'road to pedition'
-     elsif input == 'wild target'
-     elsif input == 'happy gilmore'
-     elsif input == 'madagascar'
-   else
-     'Please search for a new movie'
-   end
+    Movies.all
+    puts 'Please pick a movie.:'
+  puts '1. Titanic'
+  puts '2. V for Vendetta'
+  puts '3. atonement'
+  puts '4. road to pedition'
+  puts '5. wild target'
+  puts '6. happy gilmore'
+  puts '7. madagascar'
+  @prompt.select("Pick your number?", %w(1 2 3 4 5 6 7))
 end
+if 1
 
-def savethesong
-  puts 'Would you like to save this song?'
-song = gets.chomp
-  if song == yes
-     elsif song == no
-  else
-  'Please answer with yes or no'
 
- end
+   #
+   #  if input == 'titanic'
+   #   elsif input == 'v for vendetta'
+   #   elsif input == 'atonement'
+   #   elsif input == 'road to pedition'
+   #   elsif input == 'wild target'
+   #   elsif input == 'happy gilmore'
+   #   elsif input == 'madagascar'
+   # else
+   #   'Please search for a new movie'
+   # end
+
+
+  def savethesong
+    puts 'Would you like to save this song?'
+    song = gets.chomp
+    if song == yes
+       elsif song == no
+    else
+    'Please answer with yes or no'
+    end
+
+   end
 
 
   def end_the_app
     puts "okay, byeeeeee!"
     system exit
+  end
+
+  def run
+    find_or_create_user
+    welcome
+    movieselection
+    end_the_app
   end
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,2 @@
 require_relative '../config/environment'
-
-cli = CLI.new
-
-cli.welcome
-cli.movieselection
-
-
-
-cli.end_the_app
-
 # puts "hello world"
